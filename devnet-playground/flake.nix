a75c789498b81be1b22e0f6c7c5205d628cd44ff
akrem <akremstudy@gmail.com>
2023-01-03 09:49:18 -0500

    Add devshell.toml and nix files

---
+++ devnet-playground/flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pact-nix = {
+      url = "github:thomashoneyman/pact-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    pact-nix,
+    flake-utils,
+    devshell,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pactOverlay = _: _: {
+        pact = pact-nix.packages.${system}.pact;
+      };
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [pactOverlay devshell.overlay];
+      };
+    in {
+      devShell = pkgs.devshell.mkShell {
+        imports = [(pkgs.devshell.importTOML ./devshell.toml)];
+      };
+    });
+}
+
